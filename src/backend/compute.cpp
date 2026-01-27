15485604a24bb35952a2307c53c9c7621759e142
Alexander <ajroot@stanford.edu>
2026-01-26 23:15:57 -0800

    fix for loop optimization with lookup

--- src/backend/compute.cpp
+++ src/backend/compute.cpp
@@ -841,9 +841,9 @@ llir::lStmt ComputeFunctionLowerer::lower_loop(
             }
 
             if( body.defined()) {
-                body = llir::Sequence::make({std::move(body), make_body(is[0])});
+                body = llir::Sequence::make({std::move(body), make_body(s)});
             } else {
-                body = make_body(is[0]);
+                body = make_body(s);
             }
             
             // TODO: load index if sparse iterator
